4d7ccdc8db5ca11e4b5e7e0185c48b6db991ba42
Richard Benson <me@richardbenson.co.uk>
2011-03-28 20:34:37 +0100

    rename addNode to avoid confusion, tidy some tab stops. getNodes, getNode functions

--- Cloud/LoadBalancer.php
+++ Cloud/LoadBalancer.php
@@ -63,25 +63,25 @@ class Cloud_LoadBalancer extends Cloud {
         return false;
     }
 	
-	/**
-	 * Adds a node before creating a balancer
-	 *
-	 * @param string $address IP Address of the node
-	 * @param int $port local port on the node
-	 * @param strong $condition Whether the node is enabled
-	 */
-	 public function addNode ($address, $port, $condition = true)
-	 {
-		if($condition) {
-			$condition = "ENABLED";
-		} else {
-			$condition = "DISABLED";
-		}
-		array_push ($this->_apiNodes, array('address' => $address,
-											'port' => (string)$port,
-											'condition' => $condition));
-		return $this->_apiNodes;
-	 }
+    /**
+     * Adds a node before creating a balancer
+     *
+     * @param string $address IP Address of the node
+     * @param int $port local port on the node
+     * @param strong $condition Whether the node is enabled
+     */
+     public function newNode ($address, $port, $condition = true)
+     {
+        if($condition) {
+                $condition = "ENABLED";
+        } else {
+                $condition = "DISABLED";
+        }
+        array_push ($this->_apiNodes, array('address' => $address,
+                                            'port' => (string)$port,
+                                            'condition' => $condition));
+        return $this->_apiNodes;
+     }
 	
 
     /**
@@ -90,13 +90,13 @@ class Cloud_LoadBalancer extends Cloud {
      * @param string $name balancer name, must be unique
      * @param int $port external facing port balancer will use
      * @param string $protocol protocol for the balancer
-	 * @param string $virtualIP PUBLIC, SERVICENET or ID of existing IP
+     * @param string $virtualIP PUBLIC, SERVICENET or ID of existing IP
      * @return mixed returns json string of balancer's configuration or false on failure
      */
     public function createBalancer ($name, $port, $protocol, $virtualIp = "PUBLIC")
     {
-		// We have to have nodes set up to create a balancer
-		if (count($this->_apiNodes) == 0) return false;
+        // We have to have nodes set up to create a balancer
+        if (count($this->_apiNodes) == 0) return false;
 		
         // Since Rackspace automaticly removes all spaces/non alpha-numeric characters
         // let's do this on our end before submitting data
@@ -112,32 +112,32 @@ class Cloud_LoadBalancer extends Cloud {
             }
         }
 		
-		if ($virtualIp === "PUBLIC" || $virtualIp === "SERVICENET") {
-			$Ips = array(array('type' => $virtualIp));
-		} else {
-			$Ips = array(array('id' => $virtualIp));
-		}
+        if ($virtualIp === "PUBLIC" || $virtualIp === "SERVICENET") {
+                $Ips = array(array('type' => $virtualIp));
+        } else {
+                $Ips = array(array('id' => $virtualIp));
+        }
 
         $this->_apiResource = '/loadbalancers';
         $this->_apiJson = array ('loadBalancer' => array(
                                 'name' => $name,
                                 'port' => (string) $port,
                                 'protocol' => $protocol,
-								'virtualIps' => $Ips,
+				'virtualIps' => $Ips,
                                 'nodes' => $this->_apiNodes));
 
         $this->_doRequest(Cloud::METHOD_POST, Cloud::RESOURCE_BALANCER);
 
         if ($this->_apiResponseCode && $this->_apiResponseCode == '202') {
-			//Clear nodes
-			$this->_apiNodes = array();
+            //Clear nodes
+            $this->_apiNodes = array();
             return $this->_apiResponse;
         }
 
         return false;
     }
 
-	/**
+    /**
      * Delete Balancer
      *
      * @param int $balancerId id of balancer you wish to delete
@@ -148,7 +148,7 @@ class Cloud_LoadBalancer extends Cloud {
         $this->_apiResource = '/loadbalancers/'. (int) $balancerId;
         $this->_doRequest(Cloud::METHOD_DELETE, Cloud::RESOURCE_BALANCER);
 
-        // If server was deleted
+        // If balancer was deleted
         if ($this->_apiResponseCode && $this->_apiResponseCode == '202') {
             return true;
         }
@@ -156,6 +156,41 @@ class Cloud_LoadBalancer extends Cloud {
         return false;
     }
 
+    /**
+     * Get a list of nodes for a particular balancer
+     * 
+     * @param int $balancerId 
+     * @return mixed JSON node details or false
+     */
+    public function getNodes($balancerId) {
+        $this->_apiResource = '/loadbalancers/'.$balancerId.'/nodes.json'; // As of 25/03 API does not default to JSON on this command
+        $this->_doRequest(self::METHOD_GET, self::RESOURCE_BALANCER);
+
+        if ($this->_apiResponseCode && ($this->_apiResponseCode == '200' || $this->_apiResponseCode == '202')) {
+            return $this->_apiResponse;
+        }
+
+        return false; 
+    }
+    
+    /**
+     * Gets the details for a specific node on a specific balancer
+     * 
+     * @param int $balancerId
+     * @param int $nodeId
+     * @return mixed JSON details of specific node or false on error 
+     */
+    public function getNode($balancerId, $nodeId) {
+        $this->_apiResource = '/loadbalancers/'.$balancerId.'/nodes/'.$nodeId.'.json'; // As of 25/03 API does not default to JSON on this command
+        $this->_doRequest(self::METHOD_GET, self::RESOURCE_BALANCER);
+
+        if ($this->_apiResponseCode && ($this->_apiResponseCode == '200' || $this->_apiResponseCode == '202')) {
+            return $this->_apiResponse;
+        }
+
+        return false; 
+    }
+    
     /**
      * Retrieves list of virtual IPs on a specific balancer
      *
