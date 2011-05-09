85223a76e590c8e02a5301be5b3da1db558477cd
Richard Benson <me@richardbenson.co.uk>
2011-05-09 11:35:06 +0100

    List domains and list individual domain

--- Cloud/DNS.php
+++ Cloud/DNS.php
@@ -11,7 +11,45 @@
  */
 class Cloud_DNS extends Cloud {
 
+    /**
+     * Returns list of all domain names on the account
+     * 
+     * @param bool $isDetailed Return detailed results
+     * @param string $filter text to filter the list of domains by. NB: The wildcard is assumed from the left but not the right.
+     * @return mixed returns json string containing list of all domains on the
+     * account or false on failure 
+     */
+    public function getDomains ($isDetailed = false, $filter = '')
+    {
+        $this->_apiResource = '/domains'. ($isDetailed ? '/detail' : '');
+        if ($filter != '') $this->_apiResource .= '?name=' . $filter;
+        $this->_doRequest(self::METHOD_GET, self::RESOURCE_DNS);
+
+        if ($this->_apiResponseCode && ($this->_apiResponseCode == '200'
+           	    || $this->_apiResponseCode == '202')) {
+        	return $this->_apiResponse;
+        }
+
+        return false;
+    }
     
+    /**
+     * Returns details of a specific domain
+     * 
+     * @param int $domainId ID of the domain to get details for
+     * @return mixed JSON details of the specified domain or false on error
+     */
+    public function getDomain ($domainId)
+    {
+        $this->_apiResource = '/domains/'. (int)$domainId;
+        $this->_doRequest(self::METHOD_GET, self::RESOURCE_DNS);
+
+        if ($this->_apiResponseCode && ($this->_apiResponseCode == '200')) {
+        	return $this->_apiResponse;
+        }
+
+        return false;
+    }
     
 }
 ?>
